6ae3ac271408f1751e0361917b296eb8c308ea05
Chris Richards <chris@yellowfeather.com>
2025-08-19 23:29:37 +0100

    Fixes build errors

--- src/utils/frontmatter.ts
+++ src/utils/frontmatter.ts
@@ -1,16 +1,18 @@
 import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 import { visit } from 'unist-util-visit';
-import type { MarkdownAstroData, RehypePlugin, RemarkPlugin } from '@astrojs/markdown-remark';
+import type { RehypePlugin } from '@astrojs/markdown-remark';
 
-export const readingTimeRemarkPlugin: RemarkPlugin = () => {
-  return function (tree, file) {
+
+export function readingTimeRemarkPlugin() {
+  return function (tree, { data }) {
     const textOnPage = toString(tree);
-    const readingTime = Math.ceil(getReadingTime(textOnPage).minutes);
-
-    (file.data.astro as MarkdownAstroData).frontmatter.readingTime = readingTime;
+    const readingTime = getReadingTime(textOnPage);
+    // readingTime.text will give us minutes read as a friendly string,
+    // i.e. "3 min read"
+    data.astro.frontmatter.minutesRead = readingTime.text;
   };
-};
+}
 
 export const responsiveTablesRehypePlugin: RehypePlugin = () => {
   return function (tree) {
